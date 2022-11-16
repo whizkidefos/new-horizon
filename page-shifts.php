5d5b482f5e45db8bfa764c89b41e5b3cbe002d35
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2022-11-16 09:03:39 +0000

    Added shifts page template and changed screenshot

---
+++ page-shifts.php
@@ -0,0 +1,28 @@
+<?php /* Template Name: Accounts Page Template */ ?>
+
+<?php get_header(); ?>
+
+<section class="page-header">
+    <div class="uk-container">
+        <h1 class="uk-text-center"><?php the_title(); ?></h1>
+    </div>
+</section>
+
+<section class="page-body">
+    <div class="uk-container">
+        <article>
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+
+            <?php the_content(); ?>
+
+            <?php endwhile; else : ?>
+                <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+            <?php endif; ?>
+        </article>
+    </div>
+</section>
+
+<?php get_template_part('/components/benefits'); ?>
+
+<?php get_footer(); ?>
